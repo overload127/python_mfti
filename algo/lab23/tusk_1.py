83d7454d9c677e0d36a70d4086f925f3a3477345
Kostya Vinogradov <kostya.06.05.1995@gmaul.com>
2020-04-09 16:33:15 +0300

    Выполнена лаба 23

---
+++ algo/lab23/tusk_1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from flask import Flask
+from flask import request
+from flask import url_for
+app = Flask(__name__)
+
+log = ''
+
+@app.route('/')
+def index():
+    return app.send_static_file('client2.html')
+
+
+@app.route('/log')
+def get_log():
+    global log
+    return log
+
+@app.route('/send', methods=['POST'])
+def send():
+    global log
+    print(request.form)
+    log += request.form['msg'] + '<br>'
+    return log
+
+if __name__ == '__main__':
+    app.run()
